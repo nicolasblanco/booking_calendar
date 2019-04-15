0adc52f4a4788df01d204c7fe209bb45f3cc8381
Nicolas Blanco <nicolas@nicolasblanco.fr>
2019-04-15 22:57:03 +0300

    Update config

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :booking_calendar, BookingCalendarWeb.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "damp-journey-59555.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
